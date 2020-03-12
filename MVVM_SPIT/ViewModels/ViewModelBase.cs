c9e55d022f462add97c46018be0bfa889c078bb0
Рафаэл Гафаров <rafael.gafarov.26@gmail.com>
2020-03-12 17:51:53 +0500

    Добавьте файлы проекта.

---
+++ MVVM_SPIT/ViewModels/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_SPIT.ViewModels
+{
+    abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        protected bool Set<T>(ref T field, T value, [CallerMemberName]string propName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propName);
+            return true;
+        }
+    }
+}
